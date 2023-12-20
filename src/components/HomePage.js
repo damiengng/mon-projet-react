1df0d153d78a3c6a1435291843f1b1fa389cf71d
dmngnng <d_gennevoise@yahoo.fr>
2023-12-20 17:18:27 +0100

    colors

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,23 +1,43 @@
 // HomePage.js
 import React from 'react';
-//import axios from 'axios';
 
 function HomePage() {
     return (
-
-        <div>
-            <header>
+        <div style={containerStyle}>
+            <header style={headerStyle}>
                 <h1>Lecture des dossiers</h1>
                 <p>
                     Ici, on facilite le fait de trouver les fichiers dans un répertoire existant dans votre machine !
                     On range tout dans un dossier qui porte le même nom que l’extension de ce fichier !
                 </p>
             </header>
-            <h2>Accueil</h2>
-            <p>Bienvenue sur la page d'accueil!</p>
-
+            <div style={contentStyle}>
+                <h2>Bienvenue sur la page d'accueil!</h2>
+                <p>
+                    Bienvenue sur notre application de tri de fichiers. Utilisez le bouton ci-dessous pour trier les fichiers dans le répertoire spécifié.
+                </p>
+            </div>
         </div>
     );
 }
+
+const containerStyle = {
+    margin: '20px',
+    padding: '20px',
+    border: '1px solid #ccc',
+    borderRadius: '5px',
+};
+
+const headerStyle = {
+    background: '#333',
+    color: '#fff',
+    textAlign: 'center',
+    padding: '10px',
+    marginBottom: '10px',
+};
+
+const contentStyle = {
+    marginBottom: '10px',
+};
 
 export default HomePage;
--- src/components/TableContent.js
+++ src/components/TableContent.js
@@ -1,3 +1,4 @@
+// TableContent.js
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 
@@ -8,8 +9,8 @@ function TableContent() {
         try {
             // Exécuter l'API pour trier les fichiers
             const response = await axios.post('http://127.0.0.1:5000/sort_files', {
-                chemin_source: '/Users/damiengennevoise/Documents/M1 Paris 8/Technologies Web Internet/Projet2/mon-projet-react/tests',  // Remplacez par le chemin source approprié
-                chemin_destination: '/Users/damiengennevoise/Documents/M1 Paris 8/Technologies Web Internet/Projet2/mon-projet-react/tests'  // Remplacez par le chemin destination approprié
+                chemin_source: '/Users/damiengennevoise/Documents/M1 Paris 8/Technologies Web Internet/Projet2/mon-projet-react/tests',
+                chemin_destination: '/Users/damiengennevoise/Documents/M1 Paris 8/Technologies Web Internet/Projet2/mon-projet-react/tests'
             });
 
             console.log(response.data);  // Afficher la réponse de l'API (message de succès)
@@ -35,25 +36,80 @@ function TableContent() {
         fetchData();
     }, []);
 
+    const getColorByFileType = (fileType) => {
+        switch (fileType) {
+            case 'texte':
+                return '#3498db'; // Bleu
+            case 'pdf':
+                return '#e74c3c'; // Rouge
+            case 'image':
+                return '#2ecc71'; // Vert
+            case 'html':
+                return '#f39c12'; // Jaune
+            default:
+                return '#95a5a6'; // Gris
+        }
+    };
+
     return (
-        <div>
-            <h2>Contenu de la table</h2>
+        <div style={containerStyle}>
+            <h2 style={headerStyle}>Contenu de la table</h2>
 
             {/* Bouton pour trier les fichiers */}
-            <button onClick={sortFiles}>Trier les fichiers</button>
+            <button style={buttonStyle} onClick={sortFiles}>Trier les fichiers</button>
 
             {/* Afficher les données récupérées de la base de données */}
             {filesData.map((file, index) => (
-                <div key={index}>
-                    <p>Nom: {file.name}</p>
+                <div key={index} style={{ ...fileStyle, backgroundColor: getColorByFileType(file.file_type) }}>
+                    <p style={fileTitleStyle}>Nom: {file.name}</p>
                     <p>Taille: {file.size} Ko</p>
                     <p>Type: {file.file_type}</p>
                     <p>Chemin: {file.path}</p>
-                    <hr />
+                    <hr style={hrStyle} />
                 </div>
             ))}
         </div>
     );
 }
+
+const buttonStyle = {
+    background: '#4CAF50',
+    color: '#fff',
+    padding: '10px 15px',
+    border: 'none',
+    borderRadius: '3px',
+    cursor: 'pointer',
+    marginBottom: '10px',
+};
+
+const fileStyle = {
+    margin: '10px 0',
+    padding: '10px',
+    borderRadius: '5px',
+};
+
+const fileTitleStyle = {
+    fontWeight: 'bold',
+};
+
+const hrStyle = {
+    borderTop: '1px solid #ccc',
+    margin: '10px 0',
+};
+
+const containerStyle = {
+    margin: '20px',
+    padding: '20px',
+    border: '1px solid #ccc',
+    borderRadius: '5px',
+};
+
+const headerStyle = {
+    background: '#333',
+    color: '#fff',
+    textAlign: 'center',
+    padding: '10px',
+    marginBottom: '10px',
+};
 
 export default TableContent;
